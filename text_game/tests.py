00768abc77c8fb3b027e88494298f875b3b131bf
JamesGardner1 <46765373+JamesGardner1@users.noreply.github.com>
2019-12-19 16:04:40 -0600

    Update tests.py

--- text_game/tests.py
+++ text_game/tests.py
@@ -2,12 +2,26 @@ from django.test import TestCase
 
 # Create your tests here.
 class TestNextText(TestCase):
-
+    
+    def setup(self):
+        pass
+    
     def test_next_data_loaded(self):
-        response = self.client.get
+        self.assertTrue()
     
-    def test_next_text_data_sequence(self):
+    def tearDown(self):
+        pass
     
 
-
+    
+    
+class TestRestartGame(TestCase):
+    
+    
+    
+    
+    
+    
+    
+class TestStoryBoolean(TestCase):
     
